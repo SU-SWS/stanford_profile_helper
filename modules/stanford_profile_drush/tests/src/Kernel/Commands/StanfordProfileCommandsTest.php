69c97e29a7eb556702562e6bcf866ad8e3493ed9
Mike Decker <pookmish@gmail.com>
2020-11-13 11:00:37 -0800

    phpunit void return annoation

--- modules/stanford_profile_drush/tests/src/Kernel/Commands/StanfordProfileCommandsTest.php
+++ modules/stanford_profile_drush/tests/src/Kernel/Commands/StanfordProfileCommandsTest.php
@@ -49,7 +49,7 @@ class StanfordProfileCommandsTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
     $this->installEntitySchema('node');
     $this->installEntitySchema('user');
