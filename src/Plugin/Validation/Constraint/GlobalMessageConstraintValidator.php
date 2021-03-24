c60739359efd1dfa65e07a20917adfa1da78af1a
Ian Monroe <ian@ianmonroe.com>
2021-03-23 18:30:01 -0600

    linting

--- src/Plugin/Validation/Constraint/GlobalMessageConstraintValidator.php
+++ src/Plugin/Validation/Constraint/GlobalMessageConstraintValidator.php
@@ -8,8 +8,7 @@ use Symfony\Component\Validator\ConstraintValidator;
 /**
  * Validates the UniqueInteger constraint.
  */
-class GlobalMessageConstraintValidator extends ConstraintValidator
-{
+class GlobalMessageConstraintValidator extends ConstraintValidator {
 
   /**
    * {@inheritdoc}
@@ -19,25 +18,25 @@ class GlobalMessageConstraintValidator extends ConstraintValidator
     $is_valid = FALSE;
 
     $resource_fields = [
-        'su_global_msg_label',
-        'su_global_msg_header',
-        'su_global_msg_message',
-        'su_global_msg_link',
+      'su_global_msg_label',
+      'su_global_msg_header',
+      'su_global_msg_message',
+      'su_global_msg_link',
     ];
 
     if ($value->getEntity()->hasField('su_global_msg_enabled') &&
     $value->getEntity()->get('su_global_msg_enabled')->getValue()[0]['value']) {
 
-        foreach($resource_fields as $field) {
-            if (!empty($value->getEntity()->get($field)->getValue())) {
-                $is_valid = TRUE;
-            }
+      foreach ($resource_fields as $field) {
+        if (!empty($value->getEntity()->get($field)->getValue())) {
+          $is_valid = TRUE;
         }
+      }
 
-        if (!$is_valid) {
-            $this->context->addViolation($constraint->fieldsNotPopulated);
-        }
-        
+      if (!$is_valid) {
+        $this->context->addViolation($constraint->fieldsNotPopulated);
+      }
+
     }
   }
 
