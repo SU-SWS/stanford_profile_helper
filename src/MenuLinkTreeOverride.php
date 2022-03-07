c48f75616b10bfcfecc3958055fa0b3182f6ee6c
Mike Decker <pookmish@gmail.com>
2022-03-07 10:09:22 -0800

    Add config:system.menu cache tags to be removed

--- src/MenuLinkTreeOverride.php
+++ src/MenuLinkTreeOverride.php
@@ -35,7 +35,7 @@ class MenuLinkTreeOverride implements MenuLinkTreeInterface {
     $build['#cache']['tags'][] = 'stanford_profile_helper:menu_links';
     // Remove node cache tags since we'll use our own cache tag above.
     $build['#cache']['tags'] = array_filter($build['#cache']['tags'], function ($tag) {
-      return strpos($tag, 'node:') === FALSE;
+      return strpos($tag, 'node:') === FALSE && strpos($tag, 'config:system.menu.') === FALSE;
     });
     $build['#cache']['tags'] = array_values($build['#cache']['tags']);
     return $build;
