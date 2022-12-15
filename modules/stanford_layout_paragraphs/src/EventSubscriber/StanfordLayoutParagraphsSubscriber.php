a935369210c2bdc4e28d14a859094a13afeae8ab
Mike Decker <pookmish@gmail.com>
2022-12-15 13:50:31 -0800

    Added layout paragraphs modifier module

---
+++ modules/stanford_layout_paragraphs/src/EventSubscriber/StanfordLayoutParagraphsSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\stanford_layout_paragraphs\EventSubscriber;
+
+use Drupal\layout_paragraphs\Event\LayoutParagraphsAllowedTypesEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Stanford layout paragraphs event subscriber.
+ */
+class StanfordLayoutParagraphsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      LayoutParagraphsAllowedTypesEvent::EVENT_NAME => 'layoutParagraphsAllowedTypes',
+    ];
+  }
+
+  /**
+   * Adjust the paragraphs allowed in the layout paragraphs widget.
+   *
+   * @param \Drupal\layout_paragraphs\Event\LayoutParagraphsAllowedTypesEvent $event
+   *   Layout paragraphs event.
+   */
+  public function layoutParagraphsAllowedTypes(LayoutParagraphsAllowedTypesEvent $event): void {
+    $parent_component = $event->getLayout()
+      ->getComponentByUuid($event->getParentUuid());
+
+    // If adding a new layout, it won't have a parent.
+    if ($parent_component) {
+
+      $layout_settings = $parent_component->getSettings();
+      if ($layout_settings['layout'] != 'layout_paragraphs_1_column') {
+        $types = $event->getTypes();
+        unset($types['stanford_banner']);
+        $event->setTypes($types);
+      }
+    }
+  }
+
+}
---
+++ modules/stanford_layout_paragraphs/src/Layouts/TwoColumn.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\stanford_layout_paragraphs\Layouts;
+
+use Drupal\layout_builder\Plugin\Layout\MultiWidthLayoutBase;
+
+/**
+ * Two column layout class
+ */
+class TwoColumn extends MultiWidthLayoutBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function getWidthOptions() {
+    return [
+      '50-50' => '50% - 50%',
+      '33-67' => '33% - 67%',
+      '67-33' => '67% - 33%',
+    ];
+  }
+
+}
