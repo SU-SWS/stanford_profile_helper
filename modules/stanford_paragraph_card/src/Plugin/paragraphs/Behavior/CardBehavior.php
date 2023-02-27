c165a80d0553324af41bac18b078cd41d87433cd
Mike Decker <pookmish@gmail.com>
2023-02-27 10:08:04 -0800

    Cleanup card link style

---
+++ modules/stanford_paragraph_card/src/Plugin/paragraphs/Behavior/CardBehavior.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\stanford_paragraph_card\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\paragraphs\ParagraphsBehaviorBase;
+
+/**
+ * Teaser paragraph behaviors.
+ *
+ * @ParagraphsBehavior(
+ *   id = "su_card_styles",
+ *   label = @Translation("Card Styles"),
+ *   description = @Translation("Style options for card paragraph")
+ * )
+ */
+class CardBehavior extends ParagraphsBehaviorBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function isApplicable(ParagraphsType $paragraphs_type): bool {
+    return $paragraphs_type->id() == 'stanford_card';
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state): array {
+    $element = parent::buildBehaviorForm($paragraph, $form, $form_state);
+    $element['link_style'] = [
+      '#title' => $this->t('Link Style'),
+      '#description' => $this->t('Choose how you would like the link to display. '),
+      '#type' => 'select',
+      '#empty_option' => $this->t('Button'),
+      '#options' => [
+        'action' => $this->t('Action Link'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting('su_card_styles', 'link_style'),
+    ];
+    return $element;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function view(array &$build, ParagraphInterface $paragraph, EntityViewDisplayInterface $display, $view_mode): void {
+    $link_style = $paragraph->getBehaviorSetting('su_card_styles', 'link_style');
+    if ($link_style) {
+      // Change the DS config going in to the render.
+      $build['#ds_configuration']['regions']['card_cta_label'] = $build['#ds_configuration']['regions']['card_button_label'];
+      unset($build['#ds_configuration']['regions']['card_button_label']);
+    }
+  }
+
+}
---
+++ modules/stanford_paragraph_card/tests/src/Unit/Plugin/paragraphs/Behavior/CardBehaviorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\Tests\stanford_paragraph_card\Unit\Plugin\paragraphs\Behavior;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Form\FormState;
+use Drupal\paragraphs\Entity\ParagraphsType;
+use Drupal\paragraphs\ParagraphInterface;
+use Drupal\stanford_paragraph_card\Plugin\paragraphs\Behavior\CardBehavior;
+use Drupal\Tests\UnitTestCase;
+
+class CardBehaviorTest extends UnitTestCase {
+
+  protected function setUp(): void {
+    parent::setUp();
+    $container = new ContainerBuilder();
+    $container->set('string_translation', $this->getStringTranslationStub());
+    \Drupal::setContainer($container);
+  }
+
+  public function testLinkStyleBehavior() {
+    $paragraph_type = $this->createMock(ParagraphsType::class);
+    $paragraph_type->method('id')->willReturn($this->randomMachineName());
+    $this->assertFalse(CardBehavior::isApplicable($paragraph_type));
+
+    $paragraph_type = $this->createMock(ParagraphsType::class);
+    $paragraph_type->method('id')->willReturn('stanford_card');
+    $this->assertTrue(CardBehavior::isApplicable($paragraph_type));
+
+    $field_manager = $this->createMock(EntityFieldManagerInterface::class);
+    $behavior = new CardBehavior([], '', [], $field_manager);
+
+    $paragraph = $this->createMock(ParagraphInterface::class);
+
+    $form = [];
+    $form_State = new FormState();
+    $element = $behavior->buildBehaviorForm($paragraph, $form, $form_State);
+    $this->assertNull($element['link_style']['#default_value']);
+
+    $paragraph->method('getBehaviorSetting')->willReturn('action');
+    $element = $behavior->buildBehaviorForm($paragraph, $form, $form_State);
+    $this->assertEquals('action', $element['link_style']['#default_value']);
+
+    $display = $this->createMock(EntityViewDisplayInterface::class);
+    $build = [];
+    $build['#ds_configuration']['regions']['card_button_label'] = 'label';
+    $behavior->view($build, $paragraph, $display, 'default');
+
+    $this->assertEquals('label', $build['#ds_configuration']['regions']['card_cta_label']);
+  }
+
+}
