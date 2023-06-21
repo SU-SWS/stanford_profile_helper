32817f0df9431fa920987e329488fe4277ed22d0
Mike Decker <pookmish@gmail.com>
2023-06-21 14:09:30 -0700

    added webpack scss compiler

---
+++ modules/stanford_layout_paragraphs/webpack.config.js
@@ -0,0 +1,58 @@
+const config = require("./scss/config");
+const Webpack = require("webpack");
+const AssetsWebpackPlugin = require('assets-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+var webpackConfig = {
+  entry: {
+    "css/node_form": ["./scss/node_form.scss"],
+  },
+  output: {
+    path: config.distFolder,
+    filename: '[name].js',
+    publicPath: config.publicPath,
+    clean: true
+  },
+  mode: config.isProd ? "production" : "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          config.isProd ? { loader: MiniCssExtractPlugin.loader } : 'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ],
+      }
+    ]
+  },
+  plugins: [
+    new AssetsWebpackPlugin({path: config.distFolder}),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+    ]
+  }
+
+};
+
+if (config.hmrEnabled) {
+  webpackConfig.plugins.push(new Webpack.HotModuleReplacementPlugin());
+}
+module.exports = webpackConfig;
