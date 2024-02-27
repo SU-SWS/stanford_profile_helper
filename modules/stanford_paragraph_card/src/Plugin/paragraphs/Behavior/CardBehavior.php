88267df0cb6ecaa08e37c90cbf75c1676b8fb423
pookmish <pookmish@gmail.com>
2024-02-27 15:01:46 -0800

    D8CORE-7211 D8CORE-7210 Add header behaviors for banner and card paragraphs (#292)
    Co-authored-by: Jacob Wilson <49299083+jdwjdwjdw@users.noreply.github.com>

--- modules/stanford_paragraph_card/src/Plugin/paragraphs/Behavior/CardBehavior.php
+++ modules/stanford_paragraph_card/src/Plugin/paragraphs/Behavior/CardBehavior.php
@@ -29,8 +29,35 @@ class CardBehavior extends ParagraphsBehaviorBase {
   /**
    * {@inheritDoc}
    */
+  public function defaultConfiguration() {
+    return [
+      'heading' => 'h2',
+      'hide_heading' => FALSE,
+      'link_style' => NULL,
+    ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   public function buildBehaviorForm(ParagraphInterface $paragraph, array &$form, FormStateInterface $form_state): array {
     $element = parent::buildBehaviorForm($paragraph, $form, $form_state);
+    $element['heading'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Heading Level'),
+      '#options' => [
+        'h2' => 'H2',
+        'h3' => 'H3',
+        'h4' => 'H4',
+        'div.su-font-splash' => $this->t('Splash Text'),
+      ],
+      '#default_value' => $paragraph->getBehaviorSetting('su_card_styles', 'heading', 'h2'),
+    ];
+    $element['hide_heading'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Visually Hide Heading'),
+      '#default_value' => $paragraph->getBehaviorSetting('su_card_styles', 'hide_heading', FALSE),
+    ];
     $element['link_style'] = [
       '#title' => $this->t('Link Style'),
       '#description' => $this->t('Choose how you would like the link to display.'),
@@ -49,7 +76,7 @@ class CardBehavior extends ParagraphsBehaviorBase {
    */
   public function view(array &$build, ParagraphInterface $paragraph, EntityViewDisplayInterface $display, $view_mode): void {
     $link_style = $paragraph->getBehaviorSetting('su_card_styles', 'link_style');
-    if ($link_style ==  'action') {
+    if ($link_style == 'action') {
       // Change the DS config going in to the render.
       $build['#ds_configuration']['regions']['card_cta_label'] = $build['#ds_configuration']['regions']['card_button_label'];
       unset($build['#ds_configuration']['regions']['card_button_label']);
