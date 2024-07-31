230339fd97b2b24528f5f2c6a95569b16d2883a9
Mike Decker <pookmish@gmail.com>
2024-07-31 10:09:40 -0700

    Improve unit test

--- tests/src/Unit/Plugin/Validation/Constraint/LinkFieldItemConstraintValidatorTest.php
+++ tests/src/Unit/Plugin/Validation/Constraint/LinkFieldItemConstraintValidatorTest.php
@@ -2,86 +2,89 @@
 
 namespace Drupal\Tests\stanford_profile_helper\Unit\Plugin\Validation\Constraint;
 
+use Drupal\Core\DependencyInjection\Container;
+use Drupal\Core\Validation\ExecutionContext;
+use Drupal\Core\Validation\TranslatorInterface;
 use Drupal\stanford_profile_helper\Plugin\Validation\Constraint\LinkFieldItemConstraintValidator;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\stanford_profile_helper\Plugin\Validation\Constraint\MenuLinkItemConstraint;
+use Drupal\Tests\UnitTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Drupal\path_alias\AliasManagerInterface;
-use Symfony\Component\Validator\Context\ExecutionContextInterface;
-use Symfony\Component\Validator\Constraint;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FieldItemInterface;
-use PHPUnit\Framework\TestCase;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @coversDefaultClass \Drupal\stanford_profile_helper\Plugin\Validation\Constraint\LinkFieldItemConstraintValidator
  */
-class LinkFieldItemConstraintValidatorTest extends TestCase {
+class LinkFieldItemConstraintValidatorTest extends UnitTestCase {
 
   /**
-   * Tests the create method.
+   * Data provider for validator.
    *
-   * @covers ::create
+   * @return array[]
    */
-  public function testCreate() {
-    // Create mocks for the services.
-    $container = $this->createMock(ContainerInterface::class);
-    $request_stack = $this->createMock(RequestStack::class);
-    $alias_manager = $this->createMock(AliasManagerInterface::class);
-
-    // Configure the container mock to return the service mocks.
-    $container->expects($this->exactly(2))
-      ->method('get')
-      ->withConsecutive(['request_stack'], ['path_alias.manager'])
-      ->willReturnOnConsecutiveCalls($request_stack, $alias_manager);
-
-    // Call the create method.
-    $validator = LinkFieldItemConstraintValidator::create($container);
-
-    // Assert the returned object is an instance of LinkFieldItemConstraintValidator.
-    $this->assertInstanceOf(LinkFieldItemConstraintValidator::class, $validator);
-
-    // Assert the properties are correctly set.
-    $this->assertSame($request_stack, $validator->request);
-    $this->assertSame($alias_manager, $validator->aliasManager);
+  public function dataProvider() {
+    return [
+      ['http://localhost', 'http://localhost/foo/bar', TRUE],
+      ['http://localhost', '/foo/bar', FALSE],
+      ['http://localhost', 'http://hostlocal/foo/bar', FALSE],
+    ];
   }
 
   /**
    * Tests the validate method.
    *
-   * @covers ::validate
+   * @dataProvider dataProvider
    */
-  public function testValidate() {
+  public function testValidation($currentDomain, $linkUrl, $shouldHaveViolations) {
     // Create mocks for the services and dependencies.
     $request_stack = $this->createMock(RequestStack::class);
-    $alias_manager = $this->createMock(AliasManagerInterface::class);
-    $constraint = $this->createMock(Constraint::class);
-    $context = $this->createMock(ExecutionContextInterface::class);
+
     $field_item_list = $this->createMock(FieldItemListInterface::class);
     $field_item = $this->createMock(FieldItemInterface::class);
     $request = $this->createMock(Request::class);
 
     // Configure the request mock to return a specific scheme and host.
-    $request->method('getSchemeAndHttpHost')->willReturn('http://example.com');
+    $request->method('getSchemeAndHttpHost')->willReturn($currentDomain);
     $request_stack->method('getCurrentRequest')->willReturn($request);
 
     // Configure the field item list mock to return a field item with a specific URI.
-    $field_item->method('get')->with('uri')->willReturn('http://example.com/some/path');
+    $field_item->method('get')->willReturnSelf();
+    $field_item->method('getString')->willReturn($linkUrl);
     $field_item_list->method('get')->with(0)->willReturn($field_item);
 
+    $container = new Container();
+    $container->set('request_stack', $request_stack);
+
+    $validator = $this->createMock(ValidatorInterface::class);
+    $translator = $this->createMock(TranslatorInterface::class);
+    $translator->method('trans')->willReturnCallback(fn($message) => $message);
+    $context = new ExecutionContext($validator, NULL, $translator);
+
     // Instantiate the validator and set the context.
-    $validator = new LinkFieldItemConstraintValidator($request_stack, $alias_manager);
+    $validator = TestLinkValidator::create($container);
     $validator->initialize($context);
 
-    // Configure the constraint mock to have a specific violation message.
-    $constraint->absoluteLink = 'Absolute links are not allowed.';
-
-    // Expect the addViolation method to be called.
-    $context->expects($this->once())
-      ->method('addViolation')
-      ->with('Absolute links are not allowed.');
+    $constraint = new MenuLinkItemConstraint();
+    $context->setConstraint($constraint);
 
     // Call the validate method.
     $validator->validate($field_item_list, $constraint);
+    if ($shouldHaveViolations) {
+      $this->assertTrue($validator->hasViolation());
+    }
+    else {
+      $this->assertFalse($validator->hasViolation());
+    }
   }
+
+}
+
+class TestLinkValidator extends LinkFieldItemConstraintValidator {
+
+  public function hasViolation() {
+    return count($this->context->getViolations()) > 0;
+  }
+
 }
