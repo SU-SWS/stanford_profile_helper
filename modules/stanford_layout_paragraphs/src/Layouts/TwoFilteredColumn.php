b559b90b1108b81393ff02d227bd62e859a4f8cb
Jen Breese-Kauth <jbreese@stanford.edu>
2025-02-20 17:57:31 +0000

    D8CORE-7662: adding layouts

---
+++ modules/stanford_layout_paragraphs/src/Layouts/TwoFilteredColumn.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\stanford_layout_paragraphs\Layouts;
+
+use Drupal\layout_builder\Plugin\Layout\MultiWidthLayoutBase;
+
+/**
+ * Two column layout class.
+ */
+class TwoFilteredColumn extends MultiWidthLayoutBase {
+
+  /**
+   * {@inheritDoc}
+   *
+   * @codeCoverageIgnore Nothing to test.
+   */
+  protected function getWidthOptions() {
+    return [
+      '50-50' => 'Equal Columns',
+      '33-67' => 'Larger Right Column',
+      '67-33' => 'Larger Left Column',
+    ];
+  }
+
+}
