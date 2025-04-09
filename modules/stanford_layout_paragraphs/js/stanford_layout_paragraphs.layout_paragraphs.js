55cbd1f0e6a15855809d049639dc50f3f356b209
Mike Decker <pookmish@stanford.edu>
2025-04-09 12:33:25 -0700

    disable layout selection when using a filtered list

--- modules/stanford_layout_paragraphs/js/stanford_layout_paragraphs.layout_paragraphs.js
+++ modules/stanford_layout_paragraphs/js/stanford_layout_paragraphs.layout_paragraphs.js
@@ -1,17 +1,22 @@
-(($, Drupal ) => {
+(($, Drupal, once) => {
   Drupal.behaviors.stanfordHelperLayoutParagraphs = {
     attach: function attach(context, settings) {
       if (typeof CKEDITOR !== 'undefined') {
         Object.keys(CKEDITOR.instances).forEach(instanceId => {
           CKEDITOR.instances[instanceId].on('unlockSnapshot', snapshot => {
             snapshot.editor.fire('change');
-          })
-        })
+          });
+        });
       }
 
       // For some reason in the edit view, the two classes don't get added via
       // the template when in editing mode.
       $('.ds-entity--stanford-event .su-event-list-item', context).addClass(['su-card', 'su-event-card']);
+
+      if ($('.paragraph--type--stanford-filtered-lists').length > 0) {
+        $('#edit-layout-selection').val('stanford_basic_page_full').attr('disabled', true);
+        $(once('layout-selection-locked', '#edit-layout-selection--description')).text('Layout selection is disabled when a "filtered list" is used on the page.')
+      }
     },
   };
-})(jQuery, Drupal);
+})(jQuery, Drupal, once);
