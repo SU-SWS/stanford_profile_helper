79247b2f3a651ede90043416101d6cecc12bb0de
Mike Decker <pookmish@stanford.edu>
2025-11-11 11:43:26 -0800

    Process algolia filters data for a structured heirarchial menu

---
+++ src/Hook/AlgoliaHooks.php
@@ -0,0 +1,187 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\stanford_profile_helper\Hook;
+
+use Drupal\config_pages\ConfigPagesLoaderServiceInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\node\NodeInterface;
+use Drupal\search_api\IndexInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class AlgoliaHooks {
+
+  public function __construct(
+    protected ConfigPagesLoaderServiceInterface $configPagesLoader,
+    protected RequestStack $requestStack,
+    protected EntityTypeManagerInterface $entityTypeManager
+  ) {}
+
+  /**
+   * Implements hook_ENTITY_TYPE_update().
+   */
+  #[Hook('node_update')]
+  public function nodeUpdate(NodeInterface $node) {
+    if (
+      !$node->isPublished() &&
+      $node->getOriginal()?->isPublished()
+    ) {
+      self::clearAlgolia($node);
+    }
+  }
+
+  /**
+   * Clear the algolia index item immediately.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   Node to delete.
+   */
+  protected static function clearAlgolia(NodeInterface $node) {
+    if (\Drupal::hasService('search_api_algolia.helper')) {
+      \Drupal::service('search_api_algolia.helper')->entityDelete($node);
+    }
+  }
+
+  /**
+   * Implements hook_search_api_algolia_objects_alter().
+   */
+  #[Hook('search_api_algolia_objects_alter')]
+  public function alterObjects(array &$objects, IndexInterface $index, array $items) {
+    // If the canonical url is set, use that to adjust the urls.
+    $site_domain = $this->getSiteDomain();
+    $current_host = $this->getCurrentHost();
+    $federated = $this->isFederatedSearch();
+
+    foreach ($objects as &$item) {
+      // Move title to the beginning for more easy UI results.
+      $item = ['title' => $item['title'], ...$item];
+
+      // If the site is configured for federated search, there's a chance that
+      // two or more sites have the same UUID on the node if the sites were ever
+      // cloned. To prevent possible uuid conflicts, prefix the unique ID with a
+      // hash of the site name. Each site should have a unique site name since
+      // that is used for the refinement options.
+      if ($federated) {
+        $prefix = substr(md5($item['site_name']), 0, 5) . ':';
+        $item['objectID'] = $prefix . $item['objectID'];
+      }
+
+      // Remove fields that aren't necessary.
+      unset($item['search_api_datasource'], $item['status']);
+
+      foreach ($item as $name => &$field) {
+        // For filter fields, structure the data to work for Algolia facets.
+        // @see https://www.algolia.com/doc/api-reference/widgets/hierarchical-menu/react
+        if (str_starts_with($name, 'filters_')) {
+          // SearchAPI doesn't allow us to have multiple fields with the same
+          // key. But we will only ever have 1 filters field on each item. So
+          // make the key the same for easier re-use on the UI.
+          $item['filters'] = $this->adjustFiltersData($field);
+          unset($item[$name]);
+        }
+
+        // Data that is being sent as the taxonomy term names should always be
+        // sent as an array of strings. When the node is only configured with one
+        // term in the field, it tries to send it as a string. So we force to be
+        // an array.
+        $property_path = $index->getField($name)?->getPropertyPath() ?: '';
+        if (is_string($field) && str_contains($property_path, ':entity:name')) {
+          $field = [$field];
+        }
+
+        // Either the canonical url hasn't been set, or it matches the current
+        // request. It would match the current request when the event is happening
+        // in the UI. If cron is running, the current host won't match the canonical
+        // url.
+        if (
+          $site_domain &&
+          $site_domain != $current_host &&
+          is_string($field) &&
+          str_contains($field, $current_host)
+        ) {
+          // Change the urls from the current host to the canonical url.
+          $field = str_replace($current_host, $site_domain, $field);
+        }
+      }
+    }
+  }
+
+  /**
+   * Build a structured data for hierarchical menu with algolia.
+   *
+   * @see https://www.algolia.com/doc/api-reference/widgets/hierarchical-menu/react
+   *
+   * @param array|string $data
+   *   Either the term id or an array of term ids.
+   *
+   * @return array
+   *   Indexed array of term structure.
+   */
+  protected function adjustFiltersData($data): array {
+    $data = is_array($data) ? $data : [$data];
+    $structured_data = [];
+    $termStorage = $this->entityTypeManager->getStorage('taxonomy_term');
+
+    $terms = $termStorage->loadMultiple($data);
+
+    foreach ($terms as $term) {
+      $categories = [];
+
+      $itemData = [
+        'objectId' => $term->uuid(),
+        'name' => $term->label(),
+      ];
+
+      $level = 0;
+      while ($parent_id = $term?->get('parent')?->getString()) {
+        $parent = $termStorage->load($parent_id);
+        $lastLevel = isset($categories['categories.lvl' . $level - 1]) ? $categories['categories.lvl' . $level - 1] . ' > ' : '';
+        $categories["categories.lvl$level"] = $lastLevel . $parent->label();
+        $term = $parent;
+        $level++;
+      }
+
+      $structured_data[] = [...$itemData, ... $categories];
+    }
+    return $structured_data;
+  }
+
+  /**
+   * Get the configured canonical site domain.
+   *
+   * @return string
+   *   Canonical domain.
+   *
+   * @codeCoverageIgnore
+   */
+  protected function getSiteDomain(): string {
+    return $this->configPagesLoader->getValue('stanford_basic_site_settings', 'su_site_url', 0, 'uri');
+  }
+
+  /**
+   * Is the site configured to use a federated search.
+   *
+   * @return bool
+   *   Is federated search.
+   *
+   * @codeCoverageIgnore
+   */
+  protected function isFederatedSearch(): bool {
+    return (bool) $this->configPagesLoader->getValue('stanford_basic_site_settings', 'su_site_algolia_fed', 0, 'value');
+  }
+
+  /**
+   * Get the current host of the site.
+   *
+   * @return string|null
+   *   Current request host.
+   *
+   * @codeCoverageIgnore
+   */
+  protected function getCurrentHost(): ?string {
+    return $this->requestStack->getCurrentRequest()?->getSchemeAndHttpHost();
+  }
+
+}
---
+++ tests/src/Kernel/Hook/AlgoliaHooksTest.php
@@ -0,0 +1,372 @@
+<?php
+
+namespace Drupal\Tests\stanford_profile_helper\Kernel\EventSubscriber;
+
+use Drupal\config_pages\ConfigPagesLoaderServiceInterface;
+use Drupal\node\Entity\Node;
+use Drupal\node\Entity\NodeType;
+use Drupal\search_api\IndexInterface;
+use Drupal\search_api\Item\FieldInterface;
+use Drupal\search_api\Item\ItemInterface;
+use Drupal\search_api_algolia\SearchApiAlgoliaHelper;
+use Drupal\stanford_profile_helper\Hook\AlgoliaHooks;
+use Drupal\taxonomy\Entity\Term;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Tests\stanford_profile_helper\Kernel\SuProfileHelperKernelTestBase;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Group;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Test the Algolia hooks.
+ */
+#[Group('stanford_profile_helper')]
+#[CoversClass(AlgoliaHooks::class)]
+class AlgoliaHooksTest extends SuProfileHelperKernelTestBase {
+
+  /**
+   * The Algolia hooks service.
+   *
+   * @var \Drupal\stanford_profile_helper\Hook\AlgoliaHooks
+   */
+  protected $algoliaHooks;
+
+  /**
+   * A test node.
+   *
+   * @var \Drupal\node\NodeInterface
+   */
+  protected $node;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function register(\Drupal\Core\DependencyInjection\ContainerBuilder $container) {
+    parent::register($container);
+    // Add service alias to work around autowiring issue with Hook system.
+    $container->setAlias(
+      'Drupal\config_pages\ConfigPagesLoaderServiceInterface',
+      'config_pages.loader'
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+
+    $this->installEntitySchema('taxonomy_term');
+
+    // Create a test node type.
+    NodeType::create(['type' => 'page', 'name' => 'Page'])->save();
+
+    // Create test node.
+    $this->node = Node::create([
+      'type' => 'page',
+      'title' => 'Test Node',
+      'status' => 1,
+    ]);
+    $this->node->save();
+
+    // Mock the config pages loader service to return empty string not null.
+    $configPagesLoader = $this->createMock(ConfigPagesLoaderServiceInterface::class);
+    $configPagesLoader->method('getValue')->willReturn('');
+
+    // Create a mock request.
+    $request = Request::create('https://example.com/node/1');
+    $requestStack = $this->container->get('request_stack');
+    $requestStack->push($request);
+
+    // Create the AlgoliaHooks service with mocked dependencies.
+    $this->algoliaHooks = new AlgoliaHooks(
+      $configPagesLoader,
+      $requestStack,
+      $this->container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function tearDown(): void {
+    // Pop the request we pushed in setUp to avoid session errors.
+    $requestStack = $this->container->get('request_stack');
+    if ($requestStack->getCurrentRequest()) {
+      $requestStack->pop();
+    }
+    parent::tearDown();
+  }
+
+  /**
+   * Test node_update hook when node is unpublished.
+   */
+  public function testNodeUpdateUnpublish() {
+    // Create a mock search_api_algolia.helper service.
+    $helper = $this->createMock(SearchApiAlgoliaHelper::class);
+    $helper->expects($this->once())
+      ->method('entityDelete')
+      ->with($this->node);
+
+    $this->container->set('search_api_algolia.helper', $helper);
+
+    // Unpublish the node.
+    $this->node->setUnpublished();
+    $this->node->save();
+  }
+
+  /**
+   * Test node_update hook when node remains published.
+   */
+  public function testNodeUpdateRemainsPublished() {
+    // Create a mock that should not be called.
+    $helper = $this->createMock(SearchApiAlgoliaHelper::class);
+    $helper->expects($this->never())
+      ->method('entityDelete');
+
+    $this->container->set('search_api_algolia.helper', $helper);
+
+    // Update the node but keep it published.
+    $this->node->setTitle('Updated Title');
+    $this->node->save();
+  }
+
+  /**
+   * Test alterObjects method with basic data.
+   */
+  public function testAlterObjectsBasic() {
+    // Create a mock index.
+    $index = $this->createMock(IndexInterface::class);
+    $field = $this->createMock(FieldInterface::class);
+    $field->method('getPropertyPath')->willReturn('entity:title');
+    $index->method('getField')->willReturn($field);
+
+    $objects = [
+      [
+        'title' => 'Test Title',
+        'objectID' => 'test-uuid-123',
+        'site_name' => 'Test Site',
+        'search_api_datasource' => 'entity:node',
+        'status' => 1,
+      ],
+    ];
+
+    $items = [];
+
+    $this->algoliaHooks->alterObjects($objects, $index, $items);
+
+    // Verify title is moved to first position.
+    $keys = array_keys($objects[0]);
+    $this->assertEquals('title', $keys[0]);
+
+    // Verify unwanted fields are removed.
+    $this->assertArrayNotHasKey('search_api_datasource', $objects[0]);
+    $this->assertArrayNotHasKey('status', $objects[0]);
+  }
+
+  /**
+   * Test alterObjects with federated search enabled.
+   */
+  public function testAlterObjectsFederated() {
+    // Mock config pages loader to return federated search enabled.
+    $configPagesLoader = $this->createMock(ConfigPagesLoaderServiceInterface::class);
+    $configPagesLoader->method('getValue')
+      ->willReturnMap([
+        [
+          'stanford_basic_site_settings',
+          'su_site_algolia_fed',
+          0,
+          'value',
+          TRUE,
+        ],
+        ['stanford_basic_site_settings', 'su_site_url', 0, 'uri', ''],
+      ]);
+
+    $request = Request::create('https://example.com/node/1');
+    $requestStack = $this->container->get('request_stack');
+    $requestStack->push($request);
+
+    $algoliaHooks = new AlgoliaHooks(
+      $configPagesLoader,
+      $requestStack,
+      $this->container->get('entity_type.manager')
+    );
+
+    // Create a mock index.
+    $index = $this->createMock(IndexInterface::class);
+    $field = $this->createMock(FieldInterface::class);
+    $field->method('getPropertyPath')->willReturn('entity:title');
+    $index->method('getField')->willReturn($field);
+
+    $objects = [
+      [
+        'title' => 'Test Title',
+        'objectID' => 'test-uuid-123',
+        'site_name' => 'Test Site',
+      ],
+    ];
+
+    $items = [];
+
+    $algoliaHooks->alterObjects($objects, $index, $items);
+
+    // Pop the request.
+    $requestStack->pop();
+
+    // Verify objectID is prefixed with hash.
+    $this->assertStringContainsString(':', $objects[0]['objectID']);
+    $parts = explode(':', $objects[0]['objectID']);
+    $this->assertEquals(5, strlen($parts[0]));
+    $this->assertEquals('test-uuid-123', $parts[1]);
+  }
+
+  /**
+   * Test alterObjects with canonical URL replacement.
+   */
+  public function testAlterObjectsCanonicalUrl() {
+    // Mock config pages loader to return canonical URL.
+    $configPagesLoader = $this->createMock(ConfigPagesLoaderServiceInterface::class);
+    $configPagesLoader->method('getValue')
+      ->willReturnMap([
+        [
+          'stanford_basic_site_settings',
+          'su_site_url',
+          0,
+          'uri',
+          'https://example.stanford.edu',
+        ],
+        [
+          'stanford_basic_site_settings',
+          'su_site_algolia_fed',
+          0,
+          'value',
+          FALSE,
+        ],
+      ]);
+
+    // Set up request with different host.
+    $request = Request::create('https://test.example.com/node/1');
+    $requestStack = $this->container->get('request_stack');
+    $requestStack->push($request);
+
+    $algoliaHooks = new AlgoliaHooks(
+      $configPagesLoader,
+      $requestStack,
+      $this->container->get('entity_type.manager')
+    );
+
+    // Create a mock index.
+    $index = $this->createMock(ItemInterface::class);
+    $field = $this->createMock(FieldInterface::class);
+    $field->method('getPropertyPath')->willReturn('entity:title');
+    $index->method('getField')->willReturn($field);
+
+    $objects = [
+      [
+        'title' => 'Test Title',
+        'objectID' => 'test-uuid-123',
+        'url' => 'https://test.example.com/node/1',
+        'site_name' => 'Test Site',
+      ],
+    ];
+
+    $items = [];
+
+    $algoliaHooks->alterObjects($objects, $index, $items);
+
+    // Pop the request.
+    $requestStack->pop();
+
+    // Verify URL is replaced with canonical URL.
+    $this->assertEquals('https://example.stanford.edu/node/1', $objects[0]['url']);
+  }
+
+  /**
+   * Test alterObjects with taxonomy term field handling.
+   */
+  public function testAlterObjectsTaxonomyTermField() {
+    // Create a mock index.
+    $index = $this->createMock(IndexInterface::class);
+    $field = $this->createMock(FieldInterface::class);
+    $field->method('getPropertyPath')
+      ->willReturn('entity:field_tags:entity:name');
+    $index->method('getField')->willReturn($field);
+
+    $objects = [
+      [
+        'title' => 'Test Title',
+        'objectID' => 'test-uuid-123',
+        'site_name' => 'Test Site',
+        'field_tags' => 'Single Tag',
+      ],
+    ];
+
+    $items = [];
+
+    $this->algoliaHooks->alterObjects($objects, $index, $items);
+
+    // Verify single string is converted to array.
+    $this->assertIsArray($objects[0]['field_tags']);
+    $this->assertEquals(['Single Tag'], $objects[0]['field_tags']);
+  }
+
+  /**
+   * Test adjustFiltersData method with taxonomy terms.
+   */
+  public function testAdjustFiltersData() {
+    // Create a vocabulary.
+    Vocabulary::create([
+      'vid' => 'tags',
+      'name' => 'Tags',
+    ])->save();
+
+    // Create parent term.
+    $parent_term = Term::create([
+      'vid' => 'tags',
+      'name' => 'Parent Term',
+      'parent' => [0],
+    ]);
+    $parent_term->save();
+
+    // Create child term.
+    $child_term = Term::create([
+      'vid' => 'tags',
+      'name' => 'Child Term',
+      'parent' => [$parent_term->id()],
+    ]);
+    $child_term->save();
+
+    // Create a mock index with filters field.
+    $index = $this->createMock(IndexInterface::class);
+    $field = $this->createMock(FieldInterface::class);
+    $field->method('getPropertyPath')
+      ->willReturn('entity:field_tags:entity:tid');
+    $index->method('getField')
+      ->willReturnCallback(function($name) use ($field) {
+        return str_starts_with($name, 'filters_') ? $field : NULL;
+      });
+
+    $objects = [
+      [
+        'title' => 'Test Title',
+        'objectID' => 'test-uuid-123',
+        'site_name' => 'Test Site',
+        'filters_tags' => [$child_term->id()],
+      ],
+    ];
+
+    $items = [];
+
+    $this->algoliaHooks->alterObjects($objects, $index, $items);
+
+    // Verify filters field is created and structured correctly.
+    $this->assertArrayHasKey('filters', $objects[0]);
+    $this->assertArrayNotHasKey('filters_tags', $objects[0]);
+    $this->assertIsArray($objects[0]['filters']);
+    $this->assertNotEmpty($objects[0]['filters']);
+    $this->assertEquals('Child Term', $objects[0]['filters'][0]['name']);
+    $this->assertEquals($child_term->uuid(), $objects[0]['filters'][0]['objectId']);
+    $this->assertEquals($parent_term->label(), $objects[0]['filters'][0]['categories.lvl0']);
+  }
+
+}
