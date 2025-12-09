8e996da31001bbcd1d5039d225ff6ab80068ec8d
Mike Decker <pookmish@stanford.edu>
2025-12-09 08:03:08 -0800

    Removed configurable options

--- js/stanford_profile_helper.anchor_nav.js
+++ js/stanford_profile_helper.anchor_nav.js
@@ -2,16 +2,13 @@
   document.addEventListener('DOMContentLoaded', function (event) {
     anchors.add('#page-content h2:not(.visually-hidden)');
     let navMarkup = '';
-    const headings = document.querySelectorAll('#page-content h2:not(.visually-hidden)');
+    const headings = document.querySelectorAll('#page-content h2:not(.visually-hidden)[id]');
     for (let i = 0; i < headings.length; i++) {
       // Exclude links in the page banner since that is above the nav.
       if (headings[i].closest('.node-stanford-page-su-page-banner')) {
         continue;
       }
       const id = headings[i].getAttribute('id');
-      if (!id) {
-        continue;
-      }
       navMarkup += `<li><a href="#${id}">${headings[i].textContent}</a></li>`;
     }
 
--- src/Plugin/Block/AnchorLinkNavigationBlock.php
+++ src/Plugin/Block/AnchorLinkNavigationBlock.php
@@ -6,7 +6,6 @@ namespace Drupal\stanford_profile_helper\Plugin\Block;
 
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 
 /**
@@ -18,34 +17,6 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
   category: new TranslatableMarkup('Stanford Profile Helper'),
 )]
 final class AnchorLinkNavigationBlock extends BlockBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function defaultConfiguration(): array {
-    return [
-      'example' => $this->t('Hello world!'),
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state): array {
-    $form['example'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Example'),
-      '#default_value' => $this->configuration['example'],
-    ];
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state): void {
-    $this->configuration['example'] = $form_state->getValue('example');
-  }
 
   /**
    * {@inheritdoc}
