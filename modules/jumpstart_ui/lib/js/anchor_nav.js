955d4bcb7ac7423fb0e6c8bdbeb130c5b37b5402
khdev <kyle@webdevbydesign.com>
2026-01-27 14:59:55 -0700

    start vertical styling/js

--- modules/jumpstart_ui/lib/js/anchor_nav.js
+++ modules/jumpstart_ui/lib/js/anchor_nav.js
@@ -29,22 +29,11 @@
 
     const $overflowItemsContainer = $('<ul>').addClass('overflow-items hidden').attr('id', 'overflow-container');
 
-    function manageOverflow() {
+    function manageOverflow(vertical = false) {
       $('button', $nav).remove();
       $('.overflow-items', $nav).remove();
 
       $overflowItemsContainer.empty()
-
-      // remove 'See More' on mobile
-      var width = $(window).width();
-      var maxMobileWidth = 1185;
-      if(width < maxMobileWidth) {
-        $expandButton.html('On This Page<i class="fa-solid fa-chevron-down"></i>');
-        $expandButton.attr('aria-label', 'On This Page');
-      } else {
-        $expandButton.html('See More<i class="fa-solid fa-chevron-down"></i>');
-        $expandButton.attr('aria-label', 'See More');
-      }
 
       // Toggle overflow items visibility on expand button click
       $expandButton.on('click', () => {
@@ -52,59 +41,98 @@
         $overflowItemsContainer.toggleClass('hidden');
       });
 
+
       const $listItems = $('li', $list);
       // Reset all list items to visible
       $listItems.removeClass('hidden');
 
-      // Check if the list overflows its container
-      if ($list[0].scrollWidth > $container[0].clientWidth - 220 || width < maxMobileWidth) {
+      if(!vertical) {
+        // remove 'See More' on mobile for horizontal
+        var width = $(window).width();
+        var maxMobileWidth = 1185;
+        if(width < maxMobileWidth) {
+          $expandButton.html('On This Page<i class="fa-solid fa-chevron-down"></i>');
+          $expandButton.attr('aria-label', 'On This Page');
+        } else {
+          $expandButton.html('See More<i class="fa-solid fa-chevron-down"></i>');
+          $expandButton.attr('aria-label', 'See More');
+        }
 
-        $listItems.get().reverse().map(item => {
-          const $item = $(item);
+        // Check if the list overflows its container
+        if ($list[0].scrollWidth > $container[0].clientWidth - 220 || width < maxMobileWidth) {
 
-          if ($list[0].scrollWidth > $container[0].clientWidth - 220 || width < maxMobileWidth) {
-            $overflowItemsContainer.prepend($item.clone().addClass('overflow-item'));
-            $item.addClass('hidden');
-          }
-        });
+          $listItems.get().reverse().map(item => {
+            const $item = $(item);
 
-        $nav.append($expandButton).append($overflowItemsContainer);
+            if ($list[0].scrollWidth > $container[0].clientWidth - 220 || width < maxMobileWidth) {
+              $overflowItemsContainer.prepend($item.clone().addClass('overflow-item'));
+              $item.addClass('hidden');
+            }
+          });
+
+          $nav.append($expandButton).append($overflowItemsContainer);
+        }
+      } else {
+        // show first 7 links on vertical
+        if($listItems.length > 7) {
+
+          let i = 0;
+          $listItems.get().reverse().map(item => {
+            const $item = $(item);
+  
+            if (i < $listItems.length - 7) {
+              $overflowItemsContainer.prepend($item.clone().addClass('overflow-item'));
+              $item.remove();
+              i++;
+            }
+          });
+  
+          $nav.append($expandButton).append($overflowItemsContainer);
+          $overflowItemsContainer.addClass('vertical');
+        }
       }
+
+
+
     }
 
     if ($container.hasClass('orientation-horizontal')) {
       // Initial check and on window resize
-      manageOverflow();
+      manageOverflow(false);
       window.addEventListener('resize', manageOverflow);
-
-      window.addEventListener('keydown', event => {
-        if (event.key === 'Escape') {
-          if ($expandButton.attr('aria-expanded') === 'true') {
-            $expandButton.click();
-            $expandButton.focus();
-          }
-        }
-      });
-
-      // Event listener for clicks/focus anywhere on the document
-      document.addEventListener('click', (event) => {
-        // Check if the clicked element is outside the collapsible area or the toggle button itself (including the chevron icon)
-        const isOutsideClick = !$overflowItemsContainer[0].contains(event.target) && event.target !== $expandButton[0] && event.target.parentNode !== $expandButton[0];
-
-        if (isOutsideClick && $expandButton.attr('aria-expanded') === 'true') {
-          $expandButton.click();
-        }
-      });
-
-      // Handle focus events to close when focus moves outside
-      document.addEventListener('focusin', (event) => {
-        // Check if the newly focused element is outside the container and button
-        const isOutsideFocus = !$overflowItemsContainer[0].contains(event.target) && event.target !== $expandButton[0];
-
-        if (isOutsideFocus && $expandButton.attr('aria-expanded') === 'true') {
-          $expandButton.click();
-        }
-      });
     }
+    if ($container.hasClass('orientation-vertical')) {
+      // Initial check and on window resize
+      manageOverflow(true);
+    }
+
+    window.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        if ($expandButton.attr('aria-expanded') === 'true') {
+          $expandButton.click();
+          $expandButton.focus();
+        }
+      }
+    });
+
+    // Event listener for clicks/focus anywhere on the document
+    document.addEventListener('click', (event) => {
+      // Check if the clicked element is outside the collapsible area or the toggle button itself (including the chevron icon)
+      const isOutsideClick = !$overflowItemsContainer[0].contains(event.target) && event.target !== $expandButton[0] && event.target.parentNode !== $expandButton[0];
+
+      if (isOutsideClick && $expandButton.attr('aria-expanded') === 'true') {
+        $expandButton.click();
+      }
+    });
+
+    // Handle focus events to close when focus moves outside
+    document.addEventListener('focusin', (event) => {
+      // Check if the newly focused element is outside the container and button
+      const isOutsideFocus = !$overflowItemsContainer[0].contains(event.target) && event.target !== $expandButton[0];
+
+      if (isOutsideFocus && $expandButton.attr('aria-expanded') === 'true') {
+        $expandButton.click();
+      }
+    });
   });
 })(jQuery);
