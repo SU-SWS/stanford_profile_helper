91bf4727adaebf46403adcc6c93d5e2f4fe60e7d
Katria Lesser <katria.lesser@webdevbydesign.com>
2026-01-28 16:20:09 -0700

    reverted my change since it didn't work.

--- modules/jumpstart_ui/lib/js/anchor_nav.js
+++ modules/jumpstart_ui/lib/js/anchor_nav.js
@@ -114,10 +114,12 @@
 
     // when an anchor link is clicked, make sure to close the expanded See More menu
     function handleAnchorLinkClick(href, $li, $a, event) {
-    
+      if (typeof window.handleAnchorNavClick === 'function') {
+        window.handleAnchorNavClick(href, $li, $a, event);
+      } else {
         $expandButton.attr('aria-expanded', 'false' );
         $overflowItemsContainer.addClass('hidden');
-      
+      }
     }
 
   });
