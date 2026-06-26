a0959b8a04fed736c95ef443b1558b16de59d992
Mike Decker <pookmish@stanford.edu>
2026-06-26 12:03:38 -0700

    Added vercel protection bypass query for previews

--- modules/stanford_decoupled/src/Hook/StanfordDecoupledHooks.php
+++ modules/stanford_decoupled/src/Hook/StanfordDecoupledHooks.php
@@ -9,6 +9,7 @@ use Drupal\Core\Config\Entity\ConfigEntityInterface;
 use Drupal\Core\Field\FieldConfigInterface;
 use Drupal\Core\Hook\Attribute\Hook;
 use Drupal\node\NodeTypeInterface;
+use Drupal\stanford_decoupled\Plugin\Next\Revalidator\Path;
 
 /**
  * Hooks to help decoupled functionality.
@@ -22,6 +23,14 @@ class StanfordDecoupledHooks {
    *   Config factory service.
    */
   public function __construct(protected ConfigFactoryInterface $configFactory) {}
+
+  /**
+   * @param $plugins
+   */
+  #[Hook('next_revalidator_info_alter')]
+  public function nextRevalidatorInfoAlter(&$plugins) {
+    $plugins['path']['class'] = Path::class;
+  }
 
   /**
    * Set graphql settings when a node bundle is created.
--- modules/stanford_decoupled/src/Plugin/Next/PreviewUrlGenerator/SimplePreview.php
+++ modules/stanford_decoupled/src/Plugin/Next/PreviewUrlGenerator/SimplePreview.php
@@ -3,8 +3,10 @@
 namespace Drupal\stanford_decoupled\Plugin\Next\PreviewUrlGenerator;
 
 use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Drupal\next\Entity\NextSiteInterface;
+use Drupal\next\Plugin\ConfigurablePreviewUrlGeneratorBase;
 use Drupal\next\Plugin\PreviewUrlGeneratorBase;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -17,19 +19,46 @@ use Symfony\Component\HttpFoundation\Request;
  *  description = "Use the preview token string for the parameter."
  * )
  */
-class SimplePreview extends PreviewUrlGeneratorBase {
+class SimplePreview extends ConfigurablePreviewUrlGeneratorBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return ['vercel_bypass' => ''];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['vercel_bypass'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Vercel Preview Bypass'),
+      '#default_value' => $this->configuration['vercel_bypass'] ?? '',
+      '#description' => $this->t('Vercel preview protection bypass token. Find this in Settings → Deployment Protection of your Vercel application.'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['vercel_bypass'] = $form_state->getValue('vercel_bypass');
+  }
 
   /**
    * {@inheritdoc}
    */
   public function generate(NextSiteInterface $next_site, EntityInterface $entity, string $resource_version = NULL): ?Url {
+    $query = [
+      'slug' => $entity->toUrl()->toString(TRUE)->getGeneratedUrl(),
+      'secret' => $next_site->getPreviewSecret(),
+      'x-vercel-protection-bypass' => $this->configuration['vercel_bypass'] ?? '',
+    ];
     try {
-      return Url::fromUri($next_site->getPreviewUrl(), [
-        'query' => [
-          'slug' => $entity->toUrl()->toString(TRUE)->getGeneratedUrl(),
-          'secret' => $next_site->getPreviewSecret(),
-        ],
-      ]);
+      return Url::fromUri($next_site->getPreviewUrl(), ['query' => array_filter($query),]);
     }
     catch (\Throwable $e) {
     }
