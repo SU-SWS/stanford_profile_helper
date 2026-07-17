e7df1419c98167649784d9bd9e2a1ad432e23940
Mike Decker <pookmish@stanford.edu>
2026-07-17 14:57:12 -0700

    Refactored news vertical card and event card

--- modules/jumpstart_ui/src/Hook/JumpstartUiNodeHooks.php
+++ modules/jumpstart_ui/src/Hook/JumpstartUiNodeHooks.php
@@ -6,6 +6,9 @@ namespace Drupal\jumpstart_ui\Hook;
 
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Render\Attribute\RenderElement;
+use Drupal\Core\Render\Element;
+use Drupal\Core\Render\Renderer;
 use Drupal\node\NodeInterface;
 use function Symfony\Component\String\u;
 
@@ -16,6 +19,9 @@ class JumpstartUiNodeHooks {
    */
   #[Hook('node_view')]
   public function nodeView(array &$build, NodeInterface $node, EntityViewDisplayInterface $display, $view_mode) {
+    if ($view_mode == 'full' || $view_mode == '_custom') {
+      return;
+    }
     $bundle = u($node->bundle())->camel()->toString();
     $method = "{$bundle}View";
     if (method_exists($this, $method)) {
@@ -27,11 +33,13 @@ class JumpstartUiNodeHooks {
     $date = $node->get('su_event_date_time')->get(0)->getValue();
     [$startMonth, $startDay] = explode(' ', date('M j', (int) $date['value']));
     [$endMonth, $endDay] = explode(' ', date('M j', (int) $date['end_value']));
-    dpm($build);
+
     $build = [
       'contents' => ['#access' => FALSE, ...$build],
       'component' => [
         '#type' => 'component',
+        '#attributes' => $build['#attributes'] ?? [],
+        '#attached' => $build['#attached'] ?? [],
         '#component' => 'jumpstart_ui:event_card',
         '#props' => [
           'header_tag' => $view_mode == 'stanford_h3_card' ? 'h3' : 'h2',
@@ -44,7 +52,8 @@ class JumpstartUiNodeHooks {
           'event_type' => $build['su_event_type'] ?? NULL,
           'headline' => $build['title'] ?? NULL,
           'subheadline' => $build['su_event_subheadline'] ?? NULL,
-          'url' => $node->get('su_event_source')->getString() ?: $node->toUrl()->toString(),
+          'url' => $node->get('su_event_source')->getString() ?: $node->toUrl()
+            ->toString(),
           'date_time' => $build['su_event_date_time'] ?? NULL,
           'location' => $build['su_event_alt_loc'] ?? NULL,
           'address' => $build['su_event_location'] ?? NULL,
@@ -53,20 +62,73 @@ class JumpstartUiNodeHooks {
     ];
   }
 
-  protected function stanfordEventSeriesView(&$build, NodeInterface $node, string $view_mode) {}
+  //  protected function stanfordEventSeriesView(&$build, NodeInterface $node, string $view_mode) {}
+  //
+  //  protected function stanfordMediaView(&$build, NodeInterface $node, string $view_mode) {}
+  //
+  protected function stanfordNewsView(&$build, NodeInterface $node, string $view_mode) {
+    $build = [
+      'contents' => ['#access' => FALSE, ...$build],
+      'component' => [
+        '#type' => 'component',
+        '#component' => 'jumpstart_ui:news_vertical_teaser',
+        '#attributes' => $build['#attributes'] ?? [],
+        '#attached' => $build['#attached'] ?? [],
+        '#props' => [
+          'header_tag' => 'h3',
+        ],
+        '#slots' => [
+          'image' => $build['su_news_featured_media'] ?? NULL,
+          'headline' => $node->label(),
+          'date' => $build['su_news_publishing_date'] ?? NULL,
+          'dek' => [
+            $build['su_news_dek'] ?? NULL,
+            $build['su_news_quote'] ?? NULL,
+          ],
+          'topics' => [
+            $build['su_news_topics'] ?? NULL,
+            $build['su_news_spotlight_filters'] ?? NULL,
+          ],
+          'source' => $build['su_news_source'] ?? NULL,
+          'url' => $node->toUrl()->toString(),
+        ],
+      ],
+    ];
+  }
+  //
+  //  protected function stanfordOpportunityView(&$build, NodeInterface $node, string $view_mode) {}
+  //
+  //  protected function stanfordPageView(&$build, NodeInterface $node, string $view_mode) {}
 
-  protected function stanfordMediaView(&$build, NodeInterface $node, string $view_mode) {}
+  protected function stanfordPersonView(&$build, NodeInterface $node, string $view_mode) {
+    $attributes = $build['#attributes'] ?? [];
+    $attributes['class'][] = 'ds-entity--stanford-person';
+    $build = [
+      'contents' => ['#access' => FALSE, ...$build],
+      'component' => [
+        '#type' => 'component',
+        '#component' => 'jumpstart_ui:card',
+        '#attributes' => $attributes,
+        '#attached' => $build['#attached'] ?? [],
+        '#props' => [
+          'header_tag' => 'h3',
+        ],
+        '#slots' => [
+          'image' => $build['su_person_photo'] ?? NULL,
+          'headline' => [
+            '#type' => 'html_tag',
+            '#tag' => 'a',
+            '#value' => $node->label(),
+            '#attributes' => ['href' => $node->toUrl()->toString()],
+          ],
+          'body' => $build['su_person_short_title'] ?? NULL,
+        ],
+      ],
+    ];
+  }
 
-  protected function stanfordNewsView(&$build, NodeInterface $node, string $view_mode) {}
-
-  protected function stanfordOpportunityView(&$build, NodeInterface $node, string $view_mode) {}
-
-  protected function stanfordPageView(&$build, NodeInterface $node, string $view_mode) {}
-
-  protected function stanfordPersonView(&$build, NodeInterface $node, string $view_mode) {}
-
-  protected function stanfordPolicyView(&$build, NodeInterface $node, string $view_mode) {}
-
-  protected function stanfordPublicationView(&$build, NodeInterface $node, string $view_mode) {}
+  //  protected function stanfordPolicyView(&$build, NodeInterface $node, string $view_mode) {}
+  //
+  //  protected function stanfordPublicationView(&$build, NodeInterface $node, string $view_mode) {}
 
 }
